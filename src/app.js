e3f84c95777c220df820d914a4278fe1b9baa08d
Juan Quino <quinojuan@gmail.com>
2024-01-08 14:56:14 -0300

    structure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import express from "express"
-import morgan from "morgan"
+import express from "express";
+import morgan from "morgan";
+import indexRouter from "./routes/index.routes.js";
 
 const app = express();
 
-
 // Middlewares
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
 // Routes a utilizar
 
-// app.use("/", indexRoutes)
+app.use("/", indexRouter);
 // app.use("/api", ventasRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: "Not found" })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 export default app;

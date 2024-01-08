b50a6174d4108358e02b92809a83782f2c682c1d
quinojuan <quinojuan@gmail.com>
2024-01-08 12:10:46 -0300

    structure

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import morgan from "morgan"
+
+const app = express();
+
+
+// Middlewares
+app.use(morgan("dev"))
+app.use(express.json())
+
+// Routes
+
+// app.use("/", indexRoutes)
+// app.use("/api", ventasRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not found" })
+})
+
+export default app;

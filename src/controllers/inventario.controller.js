603740bbaa4da8f8a324bb168c9adb77e07f799b
Juan Quino <quinojuan@gmail.com>
2024-01-08 22:42:12 -0300

    calculo de inventario terminado

--- src/controllers/inventario.controller.js
+++ src/controllers/inventario.controller.js
@@ -1,6 +1,11 @@
 import { pool } from "../db.js"
 
 export const reporteDeInventario = async (req, res) => {
-  const [rows] = await pool.query(`SELECT SUM FROM productos;`)
-  res.send(rows)
+  const [rows] = await pool.query(`SELECT SUM(precio_costo * cantidad_actual) AS subtotal FROM Productos;`)
+  const {subtotal} = rows[0]
+  const [rows2] = await pool.query(`SELECT SUM(cantidad_actual) AS cantidadProductos FROM Productos;`)
+  const {cantidadProductos} = rows2[0]
+  
+  res.json(`El total del capital a la fecha es de $ ${subtotal} y es en base a ${cantidadProductos} productos`)
+
 }
